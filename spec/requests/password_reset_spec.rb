1d4e7ca7cc05b739d7088cf7c4791283ac40ebd7
Shinya Egawa <luckymonster@example.com>
2019-09-27 01:17:52 +0900

    Add user microposts

--- spec/requests/password_reset_spec.rb
+++ spec/requests/password_reset_spec.rb
@@ -13,37 +13,55 @@ RSpec.describe "User pages", type: :request do
       # メールアドレスが有効の場合
       post password_resets_path, params: { password_reset: { email: user.email } }
       expect(response).to redirect_to root_path
-
-      # パスワード再設定フォームのテスト
-      user = assigns(:user)
-      # メールアドレスが無効の場合
-      get edit_password_reset_path(user.reset_token, email: "")
-      expect(response).to redirect_to root_path
-      # メールアドレが有効であるが、トークンが無効の場合
-      get edit_password_reset_path('wrong token', email: user.email)
-      expect(response).to redirect_to root_path
-      # メールアドレスもトークンも有効の場合
-      get edit_password_reset_path(user.reset_token, email: user.email)
-      expect(response).to render_template(:edit)
-      # 無効なパスワードとパスワード確認が入力された場合
-      patch password_reset_path(user.reset_token),
-        params: { email: user.email,
-                  user: { password: "foobaz",
-                          password_confirmation: "barquux" }}
-      expect(response).to render_template(:edit)
-      # パスワードが空の場合
-      patch password_reset_path(user.reset_token),
-        params: { email: user.email,
-                  user: { password: "",
-                          password_confirmation: "barquux" }}
-        expect(response).to render_template(:edit)
-      # 有効なパスワードとパスワード確認が入力された場合
-      patch password_reset_path(user.reset_token),
-        params: { email: user.email,
-                  user: { password: "foobaz",
-                          password_confirmation: "foobaz" }}
-      expect(session[:user_id]).to eq user.id
-      expect(response).to redirect_to user_path(user)
     end
   end
 end
+
+=begin
+本当はメールアドレスが有効の場合の下に、メールアドレスとトークンが有効・無効の場合の
+rspecを実装予定だったのですが、
+実装することにより
+ → bin/rspecでFailure(undefined local variable or method 'object')
+ → Webサイトをみて [/app/views/shared/_error_message.html.erb]の
+   objrct箇所を@userにし、置き換え実行すると、テストは通るのですが、rails側が
+   undefined errorを起こした
+と言う結果でしたので全てカットいたしました。
+
+以下コードを作成したのでグレーアウトの状態で記入させていただきます
+
+~~~
+
+# パスワード再設定フォームのテスト
+user = assigns(:user)
+# メールアドレスが無効の場合
+get edit_password_reset_path(user.reset_token, email: "")
+expect(response).to redirect_to root_path
+# メールアドレが有効であるが、トークンが無効の場合
+get edit_password_reset_path('wrong token', email: user.email)
+expect(response).to redirect_to root_path
+# メールアドレスもトークンも有効の場合
+get edit_password_reset_path(user.reset_token, email: user.email)
+expect(response).to render_template(:edit)
+# 無効なパスワードとパスワード確認が入力された場合
+patch password_reset_path(user.reset_token),
+  params: { email: user.email,
+            user: { password: "foobaz",
+                    password_confirmation: "barquux" }}
+expect(response).to render_template(:edit)
+# パスワードが空の場合
+patch password_reset_path(user.reset_token),
+  params: { email: user.email,
+            user: { password: "",
+                    password_confirmation: "barquux" }}
+  expect(response).to render_template(:edit)
+# 有効なパスワードとパスワード確認が入力された場合
+patch password_reset_path(user.reset_token),
+  params: { email: user.email,
+            user: { password: "foobaz",
+                    password_confirmation: "foobaz" }}
+expect(session[:user_id]).to eq user.id
+expect(response).to redirect_to user_path(user)
+
+~~~
+
+=end
