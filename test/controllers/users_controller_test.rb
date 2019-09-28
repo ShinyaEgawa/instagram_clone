6e51f3f69349997dcbc3f7a4651f302c87848c5d
Shinya Egawa <luckymonster@example.com>
2019-09-28 19:03:26 +0900

    finish restore

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,17 +5,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user       = users(:michael)
     @other_user = users(:archer)
   end
-  
+
   test "should redirect index when not logged in" do
     get users_path
     assert_redirected_to login_url
   end
-  
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
-  
+
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
@@ -28,7 +28,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
@@ -43,7 +43,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
+
   test "should redirect following when not logged in" do
     get following_user_path(@user)
     assert_redirected_to login_url
